a6a76a04e47a82a06b14b971efc392fbdadf7205
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-05-09 16:41:11 +0545

    model and confirmation

--- Lane_object_detection.py
+++ Lane_object_detection.py
@@ -356,7 +356,7 @@ def process_image(img):
 
 
 # For video clip or real-time
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("pv.mp4")
 
 #output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') # codec
