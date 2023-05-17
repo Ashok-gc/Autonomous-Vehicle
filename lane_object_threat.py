f9ea3798965d2cb2a57d2bb017ece2128f103f54
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-05-17 13:35:52 +0545

    update 2

--- lane_object_threat.py
+++ lane_object_threat.py
@@ -385,7 +385,7 @@ def process_image(img):
 
 
 # For video clip or real-time
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("pv.mp4")
 
 #output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') # codec
